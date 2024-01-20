ab6b1eccb0fa641676566f739728364f18e4b8e9
Sergei Lind <lind-2004@mail.ru>
2024-01-20 15:43:34 +0300

    Add GameStateChanger.cs

---
+++ Snake.Unity/Assets/_Project/Develop/PlayForge Team/Snake/Runtime/GameStateChanger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace PlayForge_Team.Snake.Runtime
+{
+    public sealed class GameStateChanger : MonoBehaviour
+    {
+        [SerializeField] private GameField gameField;
+        [SerializeField] private Snakes.Snake snake;
+
+        private void Start()
+        {
+            FirstStartGame();
+        }
+
+        private void FirstStartGame()
+        {
+            gameField.FillCellsPositions();
+            snake.CreateSnake();
+        }
+    }
+}
